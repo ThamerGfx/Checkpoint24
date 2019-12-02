1ab56865cfd83f6ce195c58a4a009b3781560ec6
ThamerGfx <36084619+ThamerGfx@users.noreply.github.com>
2019-12-02 12:02:25 +0100

    Add files via upload

---
+++ mymodule.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = function(dir, ext, callback) {
+  const extension = "." + ext;
+  fs.readdir(dir, (error, list) => {
+    if (error) {
+      return callback(error);
+    }
+    list = list.filter(item => {
+      return path.extname(item) === extension;
+    });
+    callback(null, list);
+  });
+};
